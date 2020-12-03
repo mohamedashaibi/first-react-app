af237280765251857dc0306b626d0a91c71db44e
Mohamed Ashaibi <mohashaibi1@gmail.com>
2020-12-03 12:01:18 +0200

    This is the first commit to this repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import 'bootstrap/dist/css/bootstrap.css'
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <form onSubmit="" className="form-group" id="login-form">
+        <div class="form-group">
+          <label for=""></label>
+          <input type="text"
+            class="form-control" name="" id="" aria-describedby="helpId" placeholder="Username"/>
+        </div>
+        <div class="form-group">
+          <label for=""></label>
+          <input type="password"
+            class="form-control" name="" id="" aria-describedby="helpId" placeholder="Password"/>
+        </div>
+        <button type="submit" class="btn btn-primary">Log in</button>
+      </form>
+  )
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
